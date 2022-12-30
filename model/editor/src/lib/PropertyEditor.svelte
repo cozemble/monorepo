fc66f8426f6961f8d4fa99190d867b54919066b0
mikehogan <me@mikehogan.net>
2022-12-30 14:25:48 +0000

    trying to fix windows build issue

---
+++ model/editor/src/lib/PropertyEditor.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+    import type {Property} from "@cozemble/model-core";
+    import {propertyRegistry} from "@cozemble/model-core";
+    import {propertyConfigurerRegistry} from "@cozemble/model-assembled";
+    import {editorHost, emptyFormErrorState} from "@cozemble/model-editor-sdk";
+    import {writable} from 'svelte/store'
+    import {afterUpdate} from 'svelte'
+
+    export let property:Property
+
+    const formSectionErrorState = writable(emptyFormErrorState())
+    editorHost.setErrorState(formSectionErrorState)
+
+    let propertyDescriptor: PropertyDescriptor | null = null
+
+    function propertyTypeChanged(event: Event) {
+        const target = event.target as HTMLSelectElement
+        propertyDescriptor = propertyRegistry.get(target.value)
+        if (propertyDescriptor) {
+            property = {...propertyDescriptor.newProperty(), id: property.id, name: property.name}
+        } else {
+            property = {...property, type: ""}
+        }
+    }
+
+    afterUpdate(() => console.log({property, errors}))
+
+    $: configurer = propertyConfigurerRegistry.get(property._type)
+    $: errors = propertyDescriptor?.validate(property) ?? new Map()
+
+    function saveClicked() {
+        const errors = propertyDescriptor?.validate(property) ?? new Map()
+        console.log({errors, propertyDescriptor})
+        $formSectionErrorState.showErrors = errors.size > 0
+    }
+
+</script>
+
+
+<form>
+    <label>Property Name</label><br/>
+    <input bind:value={property.name}/><br/>
+    <label>Property Type</label><br/>
+    <select on:change={propertyTypeChanged}>
+        <option value="">----</option>
+        {#each propertyRegistry.list() as registeredProperty}
+            <option value={registeredProperty.id}>{registeredProperty.name.name}</option>
+        {/each}
+    </select><br/>
+    {#if configurer}
+        <svelte:component this={configurer} property={property}/>
+    {/if}
+</form>
+<button on:click|preventDefault={saveClicked} disabled={errors.size > 0}>Save</button>
--- model/editor/src/routes/propertyEditor/+page.svelte
+++ model/editor/src/routes/propertyEditor/+page.svelte
@@ -1,18 +1,11 @@
 <script lang="ts">
     import {
-        propertyConfigurerRegistry,
-        propertyRegistry,
         registerAllProperties,
         registerAllPropertyConfigurers
     } from "@cozemble/model-assembled";
-    import type Property from "@cozemble/model-core";
-    import {afterUpdate, onMount} from 'svelte'
-    import {editorHost, emptyFormErrorState} from "@cozemble/model-editor-sdk";
-    import {writable} from 'svelte/store'
-    import type {PropertyDescriptor} from "@cozemble/model-core";
-
-    const formSectionErrorState = writable(emptyFormErrorState())
-    editorHost.setErrorState(formSectionErrorState)
+    import {onMount} from 'svelte'
+    import PropertyEditor from "$lib/PropertyEditor.svelte";
+    import type {Property} from "@cozemble/model-core";
 
     let mounted = false
     onMount(() => {
@@ -26,44 +19,9 @@
         id: "1",
         name: ""
     }
-    let propertyDescriptor: PropertyDescriptor | null = null
 
-    function propertyTypeChanged(event: Event) {
-        const target = event.target as HTMLSelectElement
-        propertyDescriptor = propertyRegistry.get(target.value)
-        if (propertyDescriptor) {
-            property = {...propertyDescriptor.newProperty(), id: property.id, name: property.name}
-        } else {
-            property = {...property, type: ""}
-        }
-    }
-
-    afterUpdate(() => console.log({property, errors}))
-
-    $: configurer = propertyConfigurerRegistry.get(property._type)
-    $: errors = propertyDescriptor?.validate(property) ?? new Map()
-
-    function saveClicked() {
-        const errors = propertyDescriptor?.validate(property) ?? new Map()
-        console.log({errors, propertyDescriptor})
-        $formSectionErrorState.showErrors = errors.size > 0
-    }
 </script>
 
 {#if mounted}
-    <form>
-        <label>Property Name</label><br/>
-        <input bind:value={property.name}/><br/>
-        <label>Property Type</label><br/>
-        <select on:change={propertyTypeChanged}>
-            <option value="">----</option>
-            {#each propertyRegistry.list() as registeredProperty}
-                <option value={registeredProperty.id}>{registeredProperty.name.name}</option>
-            {/each}
-        </select><br/>
-        {#if configurer}
-            <svelte:component this={configurer} property={property}/>
-        {/if}
-    </form>
-    <button on:click|preventDefault={saveClicked} disabled={errors.size > 0}>Save</button>
+    <PropertyEditor {property} />
 {/if}
