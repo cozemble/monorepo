ac6758f8b9f25a9066c2e6cfe352dc2d3805d474
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2023-01-21 16:51:13 +0300

    data-paginated-editor: configure colors and fonts

--- data/paginated-editor/src/routes/+page.svelte
+++ data/paginated-editor/src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<a href="paginatedEditor">Paginated editor</a><br />
-<a href="paginatedEditorConnectedToLocalDb"
+<a href="paginatedEditor" class="btn">Paginated editor</a><br />
+<a href="paginatedEditorConnectedToLocalDb" class="btn btn-secondary"
   >Paginated editor connected to local db</a
 ><br />
--- data/paginated-editor/tailwind.config.cjs
+++ data/paginated-editor/tailwind.config.cjs
@@ -3,6 +3,26 @@ module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ['DM Sans'],
+    },
   },
   plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        cozemble: {
+          primary: '#141339',
+          secondary: '#66999B',
+          accent: '#dbfc6f',
+          neutral: '#324168',
+          'base-100': '#EAEDF6',
+          info: '#546AD9',
+          success: '#27A061',
+          warning: '#F6D637',
+          error: '#FB5353',
+        },
+      },
+    ],
+  },
 }
