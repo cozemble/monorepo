519678e508918bf75028f74a2bcc3edd653fc538
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2023-01-28 16:37:22 +0300

    data-editor: mock model

---
+++ frontend/data-editor/src/lib/mock/models.ts
@@ -0,0 +1,49 @@
+import type { Schema } from 'jsonschema'
+
+export const invoiceModel: Schema = {
+  type: 'object',
+  properties: {
+    invoiceNumber: {
+      type: 'string',
+      description: 'user name',
+    },
+    customers: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          firstName: {
+            type: 'string',
+          },
+          lastName: {
+            type: 'string',
+          },
+          email: {
+            type: 'string',
+            pattern: '^\\w+@[a-zA-Z_]+?\\.[a-zA-Z]{2,3}$',
+          },
+          addresses: {
+            type: 'array',
+            items: {
+              type: 'object',
+              properties: {
+                line1: {
+                  type: 'string',
+                },
+                line2: {
+                  type: 'string',
+                },
+                postcode: {
+                  type: 'string',
+                },
+              },
+              required: ['line1', 'postcode'],
+            },
+          },
+        },
+        required: ['firstName', 'email', 'addresses'],
+      },
+    },
+  },
+  required: ['invoiceNumber'],
+}
