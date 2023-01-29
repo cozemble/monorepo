8489516430f46ae68a9c9846fe3744c4be69c32c
mikehogan <me@mikehogan.net>
2023-01-29 13:52:59 +0000

    permit sign in using github

--- frontend/main-app/src/routes/+page.svelte
+++ frontend/main-app/src/routes/+page.svelte
@@ -1,8 +1,41 @@
 <script lang="ts">
-    import { createClient } from '@supabase/supabase-js'
-    import { PUBLIC_SUPABASE_KEY } from '$env/static/public';
+    import {type AuthUser, createClient} from '@supabase/supabase-js'
+    import {onMount} from 'svelte'
+    import {PUBLIC_SUPABASE_KEY} from '$env/static/public';
+
     const supabaseUrl = 'https://hxtxpwuuosksrtzditay.supabase.co'
     const supabase = createClient(supabaseUrl, PUBLIC_SUPABASE_KEY)
-    console.log({supabase})
+
+    async function signInUsingGithub() {
+        const {data, error} = await supabase.auth.signInWithOAuth({
+            provider: 'github'
+        })
+    }
+
+    let user: AuthUser | null = null
+    let mounted = false
+    let anError: any = null
+    onMount(async () => {
+        const {data, error} = await supabase.auth.getUser()
+        if (error) {
+            anError = error
+        } else {
+            user = data.user
+        }
+        mounted = true
+    })
 </script>
 <h1>Welcome to Cozemble App</h1>
+{#if anError}
+    <h2>There was an error: {anError.message}</h2>
+{/if}
+{#if mounted}
+    {#if user}
+        <h2>Hello {user.email}</h2>
+    {:else}
+        <h2>Hello Guest</h2>
+        <button type="button" on:click={signInUsingGithub}>Sign in using Github</button>
+    {/if}
+{:else}
+    <h2>Loading...</h2>
+{/if}
