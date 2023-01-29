cc94645afde03352715c563d58db9195d55929d8
mikehogan <me@mikehogan.net>
2023-01-29 11:47:02 +0000

    can I boot the supabase client

--- frontend/main-app/src/routes/+page.svelte
+++ frontend/main-app/src/routes/+page.svelte
@@ -1,1 +1,8 @@
+<script lang="ts">
+    import { createClient } from '@supabase/supabase-js'
+    import { PUBLIC_SUPABASE_KEY } from '$env/static/public';
+    const supabaseUrl = 'https://hxtxpwuuosksrtzditay.supabase.co'
+    const supabase = createClient(supabaseUrl, PUBLIC_SUPABASE_KEY)
+    console.log({PUBLIC_SUPABASE_KEY,supabase})
+</script>
 <h1>Welcome to Cozemble App</h1>
