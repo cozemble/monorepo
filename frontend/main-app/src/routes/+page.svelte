e829ec8eb9ca58521feda16050290f8813921d8c
mikehogan <me@mikehogan.net>
2023-01-29 11:48:20 +0000

    test redeploy

--- frontend/main-app/src/routes/+page.svelte
+++ frontend/main-app/src/routes/+page.svelte
@@ -3,6 +3,6 @@
     import { PUBLIC_SUPABASE_KEY } from '$env/static/public';
     const supabaseUrl = 'https://hxtxpwuuosksrtzditay.supabase.co'
     const supabase = createClient(supabaseUrl, PUBLIC_SUPABASE_KEY)
-    console.log({PUBLIC_SUPABASE_KEY,supabase})
+    console.log({supabase})
 </script>
 <h1>Welcome to Cozemble App</h1>
