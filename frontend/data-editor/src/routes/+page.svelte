cb0a883ef596651f4ec348cb3848caff0b9b1371
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2023-02-01 17:07:35 +0300

    data-editor: input for changing model schema

--- frontend/data-editor/src/routes/+page.svelte
+++ frontend/data-editor/src/routes/+page.svelte
@@ -1,1 +1,15 @@
-<a class="btn" href="editor">Editor</a>
+<script lang="ts">
+import { selectedModel } from '$lib/stores/models'
+
+let modelInput: string = ''
+
+$: if (!!modelInput) {
+  selectedModel.set(JSON.parse(modelInput))
+}
+</script>
+
+<div class="flex flex-col gap-4 bg-base-300 p-4">
+  <h1>Provide a model (optional)</h1>
+  <textarea cols="30" rows="10" class="textarea" bind:value={modelInput} />
+  <a class="btn btn-primary" href="editor">Save And View</a>
+</div>
