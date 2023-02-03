c6dfac05b6c465ebbe92cd9488dc924bbd3ae3f5
mikehogan <me@mikehogan.net>
2023-02-03 07:49:32 +0000

    store records in local storage

--- frontend/main-app/src/routes/DataPanel.svelte
+++ frontend/main-app/src/routes/DataPanel.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
     import {allModels} from "./host";
     import DataPanelInner from './DataPanelInner.svelte'
-    import {paginatedEditorHost, records} from "./recordsHost";
+    import {bootstrapRecords, paginatedEditorHost, records} from "./recordsHost";
 
     $: actualModels = $allModels.map(m => m.model)
+
+    bootstrapRecords(window.localStorage)
+
 </script>
 {#if actualModels.length === 0}
     <p>When you have created your first model, you will be able to edit data here</p>
