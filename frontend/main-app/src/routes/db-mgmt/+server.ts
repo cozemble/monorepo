411cd5e19f0fe43fc7cc76c4c64caf8fc1685dcc
mikehogan <me@mikehogan.net>
2023-02-05 14:24:51 +0000

    learning how to deploy sveltekit functions to vercel #3

--- frontend/main-app/src/routes/db-mgmt/+server.ts
+++ frontend/main-app/src/routes/db-mgmt/+server.ts
@@ -1,3 +1,6 @@
+import { random } from '@cozemble/lang-util'
+
 export function GET() {
-  return new Response('Hello world!', { status: 200 })
+  const guess = random.intBetween(0, 1000)
+  return new Response(`The random number is ${guess}`, { status: 200 })
 }
