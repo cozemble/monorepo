31466d987117fadfd6f4775eec727a67747b070c
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2023-02-15 20:37:20 +0300

    data-editor: create LoadingButton

---
+++ frontend/data-editor/src/lib/components/LoadingButton.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+export let action: Promise<void>
+export let properties: Record<string, any> | undefined = undefined
+
+let loading = false
+
+const onClick = async () => {
+  loading = true
+
+  await action
+
+  loading = false
+}
+</script>
+
+<button
+  class="btn btn-secondary self-end"
+  class:loading
+  on:click={onClick}
+  disabled={loading}
+  {...properties}
+>
+  <slot />
+</button>
--- frontend/data-editor/src/routes/editor/+page.svelte
+++ frontend/data-editor/src/routes/editor/+page.svelte
@@ -4,9 +4,7 @@ import { currentRecord, addRecord } from '$lib/stores/records'
 import { errors } from '$lib/stores/errors'
 
 import ObjectEditor from '$lib/components/ObjectEditor.svelte'
-
-$: console.info('selectedModel: ', $selectedModel)
-$: console.info('current record: ', currentRecord)
+import LoadingButton from '$lib/components/LoadingButton.svelte'
 </script>
 
 {#if $selectedModel?.properties}
@@ -17,10 +15,8 @@ $: console.info('current record: ', currentRecord)
       bind:value={$currentRecord}
       errors={$errors}
     />
-    <button
-      class="btn btn-secondary self-end"
-      on:click={() => addRecord($currentRecord)}>Save Record</button
-    >
+
+    <LoadingButton action={addRecord($currentRecord)}>Save</LoadingButton>
   </div>
 {:else}
   <div class="flex flex-col items-center justify-center h-full">
