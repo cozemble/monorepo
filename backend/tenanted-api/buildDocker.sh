07c6363066dc79dfd4a52de603851beb6f2c7dd8
mikehogan <me@mikehogan.net>
2023-02-24 14:56:50 +0000

    running on google cloud run now

--- backend/tenanted-api/buildDocker.sh
+++ backend/tenanted-api/buildDocker.sh
@@ -1,4 +1,4 @@
 ../../scripts/packaging/rewrite-workspace-deps.sh
 cp Dockerfile .dockerignore staging
 cp tsconfig.json tsconfig-cjs.json staging
-docker build -t tenanted-api .
+docker build --platform linux/amd64 -t tenanted-api .
