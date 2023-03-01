7602e57a2cde094b224853eff3d4c66e61178749
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2023-03-01 15:02:14 +0300

    data-editor: recordLog store to keep track of changes

---
+++ frontend/data-editor/src/lib/stores/recordLog.ts
@@ -0,0 +1,38 @@
+import type { Writable, Readable } from 'svelte/store'
+import { writable, derived, get } from 'svelte/store'
+import _ from 'lodash'
+
+import { currentRecord } from './records'
+
+const LOG_TIMEOUT = 1000
+
+const createHistoryLog = (record: Record<string, any>) => {
+  return {
+    timestamp: new Date().toISOString(),
+    record,
+  }
+}
+
+// * Stores
+
+export const recordLog: Writable<Record<string, any>[]> = writable([])
+
+/** Easy access to the timestamp of the last saved record */
+const lastSaved: Readable<number> = derived(recordLog, ($recordLog) => {
+  const lastLog = $recordLog[$recordLog.length - 1]
+  if (!lastLog) return new Date(0).getTime()
+
+  return new Date(lastLog.timestamp).getTime()
+})
+
+//
+
+currentRecord.subscribe((recordStore) => {
+  const record = { ...recordStore } // clone to avoid mutation
+
+  if (get(lastSaved) + LOG_TIMEOUT > new Date().getTime()) return
+
+  recordLog.update((log) => {
+    return [...log, createHistoryLog(record)]
+  })
+})
