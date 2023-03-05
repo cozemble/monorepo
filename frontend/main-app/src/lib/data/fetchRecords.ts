568110b0d234e2cad291ee55a4f6604d626d2dbd
mikehogan <me@mikehogan.net>
2023-03-05 15:15:42 +0000

    deal with more than one word in free text search

--- frontend/main-app/src/lib/data/fetchRecords.ts
+++ frontend/main-app/src/lib/data/fetchRecords.ts
@@ -1,12 +1,21 @@
 import { config } from '../config'
 
+function convertToPgQuery(s: string) {
+  return encodeURIComponent(
+    s
+      .split(' ')
+      .map((s) => `${s}:*`)
+      .join(' & '),
+  )
+}
+
 export async function fetchRecords(
   tenantId: string,
   modelId: string,
   accessToken: string,
   search: string | null = null,
 ) {
-  const q = search ? `?q=${search}` : ''
+  const q = search ? `?q=${convertToPgQuery(search)}` : ''
   return await fetch(
     `${config.backendUrl()}/api/v1/tenant/${tenantId}/model/${modelId}/record${q}`,
     {
