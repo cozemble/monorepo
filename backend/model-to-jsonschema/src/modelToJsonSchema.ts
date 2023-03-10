ee6b32ec823082febc5c75917b8d1ae48d98b4e4
mikehogan <me@mikehogan.net>
2023-03-10 08:23:21 +0000

    skipped test case to explain how jsonschema generation should work

---
+++ backend/model-to-jsonschema/src/modelToJsonSchema.ts
@@ -0,0 +1,5 @@
+import { Model } from '@cozemble/model-core'
+
+export function modelToJsonSchema(rootModel: Model, models: Model[]): string {
+  return ''
+}
---
+++ backend/model-to-jsonschema/test/example.spec.ts
@@ -0,0 +1,54 @@
+import { describe, expect, test } from 'vitest'
+import { modelFns, modelOptions, propertyFns, relationshipFns } from '@cozemble/model-api'
+import { modelToJsonSchema } from '../src/modelToJsonSchema'
+import { stringPropertyOptions } from '@cozemble/model-string-core'
+
+describe.skip('given a customer with a has-one address', () => {
+  const addressModel = modelFns.newInstance(
+    'Address',
+    modelOptions.withProperty(propertyFns.newInstance('Street')),
+    modelOptions.withProperty(propertyFns.newInstance('Post code', stringPropertyOptions.required)),
+  )
+  const customerModel = modelFns.newInstance(
+    'Customer',
+    modelOptions.withProperty(
+      propertyFns.newInstance('First name', stringPropertyOptions.required),
+    ),
+    modelOptions.withProperty(propertyFns.newInstance('Last name')),
+    modelOptions.withRelationships(relationshipFns.newInstance('Address', addressModel.id, 'one')),
+  )
+  const models = [customerModel, addressModel]
+
+  test('I can generate a json schema', () => {
+    const schema = modelToJsonSchema(customerModel, models)
+    expect(schema).toBe(`{
+    "type": "object",
+    "properties": {
+        "First name": {
+            "type": "string",
+            "description": "user name"
+        },
+        "Last name": {
+            "type": "string"
+        },
+        "Address": {
+            "type": "object",
+            "properties": {
+                "Street": {
+                    "type": "string"
+                },
+                "Post code": {
+                    "type": "string"
+                }
+            },
+            "required": [
+                "Post code"
+            ]
+        }
+    },
+    "required": [
+        "First name"
+    ]
+}`)
+  })
+})
