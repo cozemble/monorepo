e0573d25304ad20fe160df7db92bb969b4bfe5cf
Oyewole S. Abayomi <oyewoleabayomi@gmail.com>
2023-03-26 08:00:43 +0100

    fix: update property type

--- backend/model-to-jsonschema/src/modelToJsonSchema.ts
+++ backend/model-to-jsonschema/src/modelToJsonSchema.ts
@@ -107,9 +107,9 @@ const getRelationName = ({ value }: RelationshipName) => {
   return uppercamelcase(path.parse(value).name)
 }
 
-const getPropertyType = ({ value }: PropertyType) => {
+const getPropertyType = ({ type }: PropertyType) => {
   const types = Object.keys(scalarTypes)
-  const propType = value.split('.')[0]
+  const propType = type.split('.')[0]
 
   if (!types.includes(propType)) return undefined
   return propType
