268a263c01f33f1194c41dc022ad43879544cfe9
mikehogan <me@mikehogan.net>
2023-04-01 09:36:18 +0100

    rename

--- data/paginated-editor/src/lib/DataRecordTableTd.svelte
+++ data/paginated-editor/src/lib/DataRecordTableTd.svelte
@@ -2,8 +2,8 @@
     import type {DataRecordPathFocus} from './DataRecordPathFocus'
     import type {DataRecord, DataRecordPropertyPath, DataRecordPathParentElement, Property,} from '@cozemble/model-core'
     import type {Writable} from 'svelte/store'
-    import PropertyEdit from './SlotEdit.svelte'
-    import PropertyView from './SlotView.svelte'
+    import SlotEdit from './SlotEdit.svelte'
+    import SlotView from './SlotView.svelte'
     import MaybeError from './MaybeError.svelte'
     import {dataRecordPathFns} from '@cozemble/model-api'
 
@@ -26,9 +26,9 @@
 
 <td class:focussed data-record-path={dataRecordPathAsString(property)}>
     {#if focussed}
-        <PropertyEdit {parentPath} {property} {record}/>
+        <SlotEdit {parentPath} {property} {record}/>
     {:else}
-        <PropertyView {property} {record}/>
+        <SlotView {property} {record}/>
     {/if}
     <MaybeError {parentPath} {property} {errors} {showErrors}/>
 </td>
