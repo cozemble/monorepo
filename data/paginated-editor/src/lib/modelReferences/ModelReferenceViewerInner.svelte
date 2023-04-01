846ef6a059e8926558bdfae68c9ff2d118d1e1a5
mikehogan <me@mikehogan.net>
2023-04-01 14:53:27 +0100

    small tidy

--- data/paginated-editor/src/lib/modelReferences/ModelReferenceViewerInner.svelte
+++ data/paginated-editor/src/lib/modelReferences/ModelReferenceViewerInner.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
     import type {EditorParams} from "./editorHelper";
-    import type {DataRecord, DataRecordId, DataRecordValuePath} from "@cozemble/model-core";
+    import type {DataRecord, DataRecordValuePath, ReferencedRecords} from "@cozemble/model-core";
     import {dataRecordValuePathFns} from "@cozemble/model-api";
     import {dataRecordViewer} from "@cozemble/data-editor-sdk";
     import {dereference} from "./dereference";
     import {renderReference} from "./renderReference";
-    import type {ReferencedRecords} from "@cozemble/model-core";
 
     export let recordPath: DataRecordValuePath
     export let record: DataRecord
@@ -14,11 +13,9 @@
 
     let referencedRecord: DataRecord | null = null
 
-    console.log({record})
     $: referencedRecords = dataRecordValuePathFns.getValue(recordPath, record) as ReferencedRecords ?? null
     $: dereference(dataRecordViewerClient, editorParams.referencedModelId, referencedRecords, (record) => referencedRecord = record)
     $: htmlRender = renderReference(referencedRecord, editorParams)
-
 
 </script>
 
