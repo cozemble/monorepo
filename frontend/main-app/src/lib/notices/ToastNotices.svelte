ebc49ebf41001ed86803823e3e18a905b00ba56b
mikehogan <me@mikehogan.net>
2023-04-16 11:13:19 +0100

    better styling of toasts

--- frontend/main-app/src/lib/notices/ToastNotices.svelte
+++ frontend/main-app/src/lib/notices/ToastNotices.svelte
@@ -1,16 +1,60 @@
 <script lang="ts">
-    import {afterUpdate} from "svelte";
     import {toastNoticeStore} from "./toastNoticeStore";
-
-    afterUpdate(() => console.log("afterUpdate", $toastNoticeStore));
+    import { fade } from 'svelte/transition';
 </script>
 
 {#each $toastNoticeStore as toast}
-    <div class="toast toast-top toast-end">
-        <div class="alert alert-{toast.level}">
-            <div>
-                <span>{toast.message}</span>
+    <div class="toast toast-top toast-end" transition:fade>
+        {#if toast.level === 'success'}
+            <div class="alert alert-success shadow-lg">
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none"
+                         viewBox="0 0 24 24">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                              d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"/>
+                    </svg>
+                    <span>{toast.message}</span>
+                </div>
             </div>
-        </div>
+        {:else if toast.level === 'error'}
+            <div class="alert alert-error shadow-lg">
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none"
+                         viewBox="0 0 24 24">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                              d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"/>
+                    </svg>
+                    <span>{toast.message}</span>
+                </div>
+            </div>
+        {:else if toast.level === 'info'}
+            <div class="alert alert-info shadow-lg">
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                         class="stroke-current flex-shrink-0 w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                              d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                    </svg>
+                    <span>{toast.message}</span>
+                </div>
+            </div>
+        {:else if toast.level === 'warning'}
+            <div class="alert alert-warning shadow-lg">
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none"
+                         viewBox="0 0 24 24">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                              d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"/>
+                    </svg>
+                    <span>{toast.message}</span>
+                </div>
+            </div>
+        {:else}
+            <div class="alert alert-{toast.level}">
+                <div>
+                    <span>{toast.message}</span>
+                </div>
+            </div>
+        {/if}
     </div>
 {/each}
