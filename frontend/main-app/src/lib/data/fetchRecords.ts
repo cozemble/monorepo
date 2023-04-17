1dfd2c6c5d1ecb51c8d931c50cb3403d73f48f9c
mikehogan <me@mikehogan.net>
2023-04-17 08:00:23 +0100

    filtering of records matches the new api

--- frontend/main-app/src/lib/data/fetchRecords.ts
+++ frontend/main-app/src/lib/data/fetchRecords.ts
@@ -11,17 +11,13 @@ export async function fetchRecords(
   search: string | null = null,
   filters = filledFilterInstanceGroupFns.empty(),
 ) {
-  const q = search ? `?q=${search}` : ''
-  return await fetch(
-    `${config.backendUrl()}/api/v1/tenant/${tenantId}/model/${modelId}/record${q}`,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-        Authorization: `Bearer ${accessToken}`,
-      },
-      body: JSON.stringify(filterRequestPayloadFns.newInstance(null, filters)),
+  return await fetch(`${config.backendUrl()}/api/v1/tenant/${tenantId}/model/${modelId}/record`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      Authorization: `Bearer ${accessToken}`,
     },
-  )
+    body: JSON.stringify(filterRequestPayloadFns.newInstance(search, filters)),
+  })
 }
