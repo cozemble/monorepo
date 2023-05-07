fa717ec761dc709a094adb3f47cebf107a63eed8
mikehogan <me@mikehogan.net>
2023-05-07 08:40:46 +0100

    first cypress test for datatable

---
+++ frontend/datatable/cypress.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  e2e: {
+    video: false,
+    defaultCommandTimeout: 10000,
+    screenshotOnRunFailure: false,
+    retries: 2,
+  },
+})
---
+++ frontend/datatable/cypress/e2e/milestone1.cy.ts
@@ -0,0 +1,5 @@
+describe('model editor', () => {
+  it('permits change of the model name', () => {
+    cy.visit('http://localhost:5173')
+  })
+})
