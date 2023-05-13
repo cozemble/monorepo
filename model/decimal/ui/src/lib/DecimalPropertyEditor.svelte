02f62b5e23e3df514d6b20765a088cd17c7ccfb7
mikehogan <me@mikehogan.net>
2023-05-13 12:07:04 +0100

    got decimal working with Sam and ChatGPT :)

--- model/decimal/ui/src/lib/DecimalPropertyEditor.svelte
+++ model/decimal/ui/src/lib/DecimalPropertyEditor.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
     import type {DataRecord, DataRecordValuePath, SystemConfiguration} from '@cozemble/model-core'
+    import {propertyDescriptors} from "@cozemble/model-core";
     import {dataRecordEditEvents, dataRecordEditor,} from '@cozemble/data-editor-sdk'
     import type {DecimalProperty} from "@cozemble/model-decimal-core";
-    import {propertyDescriptors} from "@cozemble/model-core";
 
     export let recordPath: DataRecordValuePath
     export let record: DataRecord
@@ -12,6 +12,7 @@
     const propertyDescriptor = propertyDescriptors.mandatory(property)
 
     $: value = propertyDescriptor.getValue(systemConfiguration, property, record) ?? null
+    $: numberOfDecimalPlaces = property.numberOfDecimalPlaces
 
     const dataRecordEditorClient = dataRecordEditor.getClient()
     const initialValue = propertyDescriptor.getValue(systemConfiguration, property, record) ?? null
@@ -19,7 +20,11 @@
 
     function decimalChanged(event: Event) {
         const target = event.target as HTMLInputElement
-        const newValue = target.valueAsNumber
+        let newValue = parseFloat(target.value) as number | null
+
+        if (newValue === null || isNaN(newValue)) {
+            newValue = null
+        }
 
         if (newValue !== editableValue) {
             editableValue = newValue
@@ -35,10 +40,38 @@
         }
     }
 
+    function validateInput(event: KeyboardEvent) {
+        const target = event.target as HTMLInputElement
+        const decimalPart = target.value.split('.')[1] || ''
+
+        // Check if selectionStart is null and provide a default value of 0
+        const selectionStart = target.selectionStart ?? 0;
+
+        // allow metaKey combinations (like CMD+A, CMD+C, etc.), and functional keys like Tab, Enter, Escape, arrow keys, Home, End, PageUp, PageDown, and Insert
+        if (event.metaKey || ['Tab', 'Enter', 'Escape', 'ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'Home', 'End', 'PageUp', 'PageDown', 'Insert'].includes(event.key)) return;
+
+        // check if the pressed key was not a number, and not a decimal or control key, or if the decimal part is already at maximum length
+        if ((isNaN(Number(event.key)) && event.key !== '.' && !['Backspace', 'Delete'].includes(event.key))
+            || (event.key === '.' && target.value.includes('.'))
+            || (decimalPart.length >= numberOfDecimalPlaces && !isNaN(Number(event.key)) && selectionStart > target.value.indexOf('.'))) {
+            event.preventDefault()
+        }
+    }
+
+    function validatePaste(event: Event) {
+        const target = event.target as HTMLInputElement;
+        const decimalPart = target.value.split('.')[1] || '';
+        if(decimalPart.length > numberOfDecimalPlaces) {
+            target.value = target.value.slice(0, target.value.indexOf('.') + numberOfDecimalPlaces + 1);
+        }
+    }
+
 </script>
 
 <input
         class="input input-bordered"
-        type="number"
+        type="text"
         value={editableValue}
+        on:input={validatePaste}
+        on:keydown={validateInput}
         on:change={decimalChanged}/>
