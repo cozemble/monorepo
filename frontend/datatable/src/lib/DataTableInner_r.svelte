435234fedd5f617aa8e41555cb3567b8cc40e1d9
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2023-05-22 17:09:41 +0300

    refactor(datatable): split EditorNav from DataTableInner

--- frontend/datatable/src/lib/DataTableInner_r.svelte
+++ frontend/datatable/src/lib/DataTableInner_r.svelte
@@ -3,59 +3,17 @@
 import type { Writable } from 'svelte/store'
 import { writable } from 'svelte/store'
 
-// Cozemble
-import type { ModelId } from '@cozemble/model-core'
-
-import AddTableNavButton from './AddTableNavButton_r.svelte'
-import Modals from './Modals.svelte'
-import { modelUi } from './modelUi'
-import {
-  allEventSourcedModels,
-  allTopLevelEventSourcedModels,
-} from './stores/allModels'
 import ModelPane from './models/ModelPane.svelte'
-import ModelTab from './ModelTab_r.svelte'
-import { contextHelper } from './stores/contextHelper'
 import RecordCreatorContext from './records/creator/RecordCreatorContext.svelte'
+import Modals from './Modals.svelte'
+import EditorNav from './components/editor/EditorNav.svelte'
 
 export let navbarState: Writable<string | null> = writable(null)
 
-const isModellingPermitted = contextHelper.getPermitModelling()
-
-//
-
-function onShowModel(modelId: ModelId) {
-  navbarState.set(modelId.value)
-}
-
-function onEditModel(clicked: Event, modelIndex: number) {
-  clicked.stopPropagation()
-
-  const model = $allTopLevelEventSourcedModels[modelIndex]
-  const anchor = (clicked.target as HTMLElement).closest(
-    `.model-${modelIndex + 1}`,
-  ) as HTMLElement
-
-  if (model && anchor) {
-    modelUi.edit($allEventSourcedModels, model, anchor)
-  }
-}
-
-function onAddModel(event: CustomEvent) {
-  event.detail.modelId && onShowModel(event.detail.modelId)
-}
 //
 </script>
 
-<div class="tabs bg-base-200 rounded pb-1 pl-2">
-  {#each $allTopLevelEventSourcedModels as esModel, index}
-    <ModelTab {esModel} {index} {navbarState} {onEditModel} />
-  {/each}
-
-  {#if $isModellingPermitted}
-    <AddTableNavButton on:added={onAddModel} />
-  {/if}
-</div>
+<EditorNav {navbarState} />
 
 {#if $navbarState}
   {#key $navbarState}
---
+++ frontend/datatable/src/lib/components/editor/EditorNav.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+// types
+import type { Writable } from 'svelte/store'
+import type { ModelId } from '@cozemble/model-core'
+
+import { writable } from 'svelte/store'
+
+// stores
+import { modelUi } from '$lib/modelUi'
+import { contextHelper } from '$lib/stores/contextHelper'
+import {
+  allEventSourcedModels,
+  allTopLevelEventSourcedModels,
+} from '$lib/stores/allModels'
+// components
+import ModelTab from '$lib/ModelTab_r.svelte'
+import AddTableNavButton from '$lib/AddTableNavButton_r.svelte'
+
+export let navbarState: Writable<string | null> = writable(null)
+
+const isModellingPermitted = contextHelper.getPermitModelling()
+
+function onShowModel(modelId: ModelId) {
+  navbarState.set(modelId.value)
+}
+
+function onEditModel(clicked: Event, modelIndex: number) {
+  clicked.stopPropagation()
+
+  const model = $allTopLevelEventSourcedModels[modelIndex]
+  const anchor = (clicked.target as HTMLElement).closest(
+    `.model-${modelIndex + 1}`,
+  ) as HTMLElement
+
+  if (model && anchor) {
+    modelUi.edit($allEventSourcedModels, model, anchor)
+  }
+}
+
+function onAddModel(event: CustomEvent) {
+  event.detail.modelId && onShowModel(event.detail.modelId)
+}
+</script>
+
+<div class="tabs bg-base-200 rounded pb-1 pl-2">
+  {#each $allTopLevelEventSourcedModels as esModel, index}
+    <ModelTab {esModel} {index} {navbarState} {onEditModel} />
+  {/each}
+
+  {#if $isModellingPermitted}
+    <AddTableNavButton on:added={onAddModel} />
+  {/if}
+</div>
