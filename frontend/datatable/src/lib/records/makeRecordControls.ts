e4a7475dc563dcef133d99cd07b2b77cc2227bad
mikehogan <me@mikehogan.net>
2023-05-31 08:52:02 +0100

    refactor: dry the record saving code

--- frontend/datatable/src/lib/records/makeRecordControls.ts
+++ frontend/datatable/src/lib/records/makeRecordControls.ts
@@ -1,12 +1,31 @@
 import type { EventSourcedRecordGraphStore } from './EventSourcedRecordGraphStore'
 import type { RecordControls, SubGraphCollectorsByRecordId } from './RecordControls'
 import type { Writable } from 'svelte/store'
-import type { DataRecordId, Model, SystemConfiguration } from '@cozemble/model-core'
+import type {
+  DataRecordId,
+  Id,
+  Model,
+  RecordGraphEdge,
+  SystemConfiguration,
+} from '@cozemble/model-core'
+import type { JustErrorMessage } from '@cozemble/lang-util'
 import { justErrorMessage } from '@cozemble/lang-util'
 import { modelFns } from '@cozemble/model-api'
 import type { ErrorVisibilityByRecordId } from './helpers'
 import type { RecordSaver } from '../backend/Backend'
+import type { EventSourcedDataRecord } from '@cozemble/model-event-sourced'
 import { eventSourcedRecordGraphFns } from '@cozemble/model-event-sourced'
+import type { RecordSaveOutcome } from '@cozemble/data-paginated-editor'
+
+function recordSaveTimestamp(
+  lastSavedByRecordId: Writable<Map<string, number>>,
+  record: EventSourcedDataRecord,
+): void {
+  lastSavedByRecordId.update((lastSaved) => {
+    lastSaved.set(record.record.id.value, Date.now())
+    return lastSaved
+  })
+}
 
 export function makeRecordControls(
   systemConfigurationProvider: () => SystemConfiguration,
@@ -16,77 +35,58 @@ export function makeRecordControls(
   recordGraph: EventSourcedRecordGraphStore,
   lastSavedByRecordId: Writable<Map<string, number>>,
   subGraphCollectorsByRecordId: SubGraphCollectorsByRecordId,
-  newUnsavedRecords = [] as DataRecordId[],
 ): RecordControls {
+  function validateRecord(record: EventSourcedDataRecord): JustErrorMessage | null {
+    const errors = modelFns.validate(systemConfigurationProvider(), modelsProvider(), record.record)
+    errorVisibilityByRecordId.update((recordErrorsById) => {
+      recordErrorsById.set(record.record.id.value, errors.size > 0)
+      return recordErrorsById
+    })
+    if (errors.size > 0) {
+      return justErrorMessage(`${errors.size} error(s) when saving record`)
+    }
+    return null
+  }
+
+  async function performRecordSave(
+    saveFunction: (
+      newRecord: EventSourcedDataRecord,
+      edges: RecordGraphEdge[],
+      deletedEdges: Id[],
+    ) => Promise<RecordSaveOutcome>,
+    recordId: DataRecordId,
+  ): Promise<JustErrorMessage | null> {
+    const record = eventSourcedRecordGraphFns.recordWithId(recordGraph.get(), recordId)
+    const maybeError = validateRecord(record)
+    console.log({ maybeError })
+    if (maybeError) {
+      return maybeError
+    }
+
+    const saveOutcome = await saveFunction(
+      record,
+      eventSourcedRecordGraphFns.getEdgesInvolvingRecord(recordGraph.get(), recordId),
+      recordGraph.get().deletedEdges.map((e) => e.edge.id),
+    )
+    console.log({ saveOutcome })
+    if (saveOutcome._type === 'record.save.failed') {
+      return justErrorMessage(saveOutcome.errors[0] ?? 'Unknown error when saving record')
+    }
+    recordSaveTimestamp(lastSavedByRecordId, record)
+    return null
+  }
+
   return {
     async saveRecord(recordId: DataRecordId) {
       console.log('saveRecord', { recordId, subGraphCollectorsByRecordId })
-      const record = eventSourcedRecordGraphFns.recordWithId(recordGraph.get(), recordId)
-      const errors = modelFns.validate(
-        systemConfigurationProvider(),
-        modelsProvider(),
-        record.record,
-      )
-      errorVisibilityByRecordId.update((recordErrorsById) => {
-        recordErrorsById.set(record.record.id.value, errors.size > 0)
-        return recordErrorsById
-      })
-      if (errors.size > 0) {
-        return justErrorMessage(`${errors.size} error(s) when saving record`)
-      }
-      const indexOfNewUnsavedRecords = newUnsavedRecords.findIndex(
-        (id) => id.value === recordId.value,
-      )
-      if (indexOfNewUnsavedRecords >= 0) {
-        await recordSaver.saveNewRecord(
-          record,
-          eventSourcedRecordGraphFns.getEdgesInvolvingRecord(recordGraph.get(), recordId),
-          recordGraph.get().deletedEdges.map((e) => e.edge.id),
-        )
-        // remove recordId from newUnsavedRecords
-        newUnsavedRecords.splice(indexOfNewUnsavedRecords, 1)
-      } else {
-        await recordSaver.saveExistingRecord(
-          record,
-          eventSourcedRecordGraphFns.getEdgesInvolvingRecord(recordGraph.get(), recordId),
-          recordGraph.get().deletedEdges.map((e) => e.edge.id),
-        )
-      }
-      lastSavedByRecordId.update((lastSavedByRecordId) => {
-        lastSavedByRecordId.set(record.record.id.value, Date.now())
-        return lastSavedByRecordId
-      })
-      return null
+      return await performRecordSave(recordSaver.saveExistingRecord, recordId)
     },
     async saveNewRecord(recordId: DataRecordId) {
       console.log('saveNewRecord', { recordId, subGraphCollectorsByRecordId })
-      const record = eventSourcedRecordGraphFns.recordWithId(recordGraph.get(), recordId)
-      const errors = modelFns.validate(
-        systemConfigurationProvider(),
-        modelsProvider(),
-        record.record,
-      )
-      errorVisibilityByRecordId.update((recordErrorsById) => {
-        recordErrorsById.set(record.record.id.value, errors.size > 0)
-        return recordErrorsById
-      })
-      if (errors.size > 0) {
-        return justErrorMessage(`${errors.size} error(s) when saving record`)
+      const maybeError = await performRecordSave(recordSaver.saveNewRecord, recordId)
+      if (maybeError) {
+        return maybeError
       }
-      const indexOfNewUnsavedRecords = newUnsavedRecords.findIndex(
-        (id) => id.value === recordId.value,
-      )
-      await recordSaver.saveNewRecord(
-        record,
-        eventSourcedRecordGraphFns.getEdgesInvolvingRecord(recordGraph.get(), recordId),
-        recordGraph.get().deletedEdges.map((e) => e.edge.id),
-      )
-      // remove recordId from newUnsavedRecords
-      newUnsavedRecords.splice(indexOfNewUnsavedRecords, 1)
-      lastSavedByRecordId.update((lastSavedByRecordId) => {
-        lastSavedByRecordId.set(record.record.id.value, Date.now())
-        return lastSavedByRecordId
-      })
       recordGraph.appendNewRecord()
       return null
     },
