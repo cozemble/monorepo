01012fe3bca765df236a34400a5bd3adeecd2f0e
mikehogan <me@mikehogan.net>
2023-06-13 15:48:16 +0100

    no cypress tests in the repl package

--- frontend/property-repl/src/lib/properties/JsonPropertyViewer.svelte
+++ frontend/property-repl/src/lib/properties/JsonPropertyViewer.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
-    import type {JsonProperty} from "$lib/properties/JsonProperty";
 
     export let value: any | null
-    export let property: JsonProperty
 
     $: displayable = value ?? ''
 </script>
