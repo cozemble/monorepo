a165fbd3205ce2a0805876a70ebe0d6a2b8e4064
mikehogan <me@mikehogan.net>
2023-06-24 14:21:28 +0100

    add social buttons to footer

--- frontend/ai-playground/src/routes/amend/+page.svelte
+++ frontend/ai-playground/src/routes/amend/+page.svelte
@@ -3,6 +3,7 @@
   import {goto} from "$app/navigation";
   import {onMount, tick} from "svelte";
   import {autoExpand} from "$lib/generative/autoExpander";
+  import SocialButtons from "$lib/social/SocialButtons.svelte";
 
   function startOver() {
     goto("/?startOver=true")
@@ -18,9 +19,12 @@
     <ManageModels/>
 </div>
 
-<footer class="footer p-10 bg-primary-content">
+<footer class="footer p-10 bg-primary-content flex-col flex">
     <div class="flex w-full">
         <button class="btn btn-primary mx-auto btn-wide btn-lg" on:click={startOver}>Start over</button>
+    </div>
+    <div class="bg-base-300 flex w-full round h-20 rounded-2xl items-center justify-center">
+        <SocialButtons />
     </div>
 </footer>
 
