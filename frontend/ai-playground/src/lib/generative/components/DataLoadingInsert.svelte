5432b4e4414236bfaa82f43c5fed8b1c07edd8a1
mikehogan <me@mikehogan.net>
2023-07-02 07:54:20 +0100

    log the batch being applied, so I can catch empty generated data

--- frontend/ai-playground/src/lib/generative/components/DataLoadingInsert.svelte
+++ frontend/ai-playground/src/lib/generative/components/DataLoadingInsert.svelte
@@ -40,7 +40,6 @@
                             }
                         }
                     })
-                    console.log("Applying batch", {batch})
                 })
             }
         }
