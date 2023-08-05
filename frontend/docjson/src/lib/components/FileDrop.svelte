ad647b6cc9e39ebf2220dfda9eb1bb8657b76816
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2023-08-05 18:28:51 +0300

    feat(docjson): file drop input initials

---
+++ frontend/docjson/src/lib/components/FileDrop.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  import Icon from '@iconify/svelte'
+
+  export let files: FileList | null = null
+
+  // <!-- TODO handle file drop
+  const handleFileDrop = (event: DragEvent) => {
+    event.preventDefault()
+    event.stopPropagation()
+
+    files = event.dataTransfer?.files
+  }
+
+  $: if (files) console.log(files)
+</script>
+
+<div
+  class="card card-bordered shadow-lg bg-base-200 border-dashed border-4 border-accent hover:border-accent-focus p-4 transition-all duration-200 ease-in-out "
+>
+  <label
+    for="file-input"
+    class="inline cursor-pointer"
+    on:drag={handleFileDrop}
+    on:drop={handleFileDrop}
+    on:dragover={handleFileDrop}
+  >
+    <figure class="pt-10">
+      <!-- TODO determine what illustration/icon to use -->
+      <Icon icon="ic:round-upload-file" class="text-9xl text-neutral" />
+      <!-- <Icon icon="system-uicons:document-stack" class="text-9xl text-neutral" /> -->
+      <!-- <Icon icon="heroicons:document-text-solid" class="text-9xl text-neutral" /> -->
+      <!-- <Icon icon="fxemoji:documenttextpicture" class="text-9xl " /> -->
+    </figure>
+
+    <!-- TODO display dropped file name and preview -->
+
+    <div class="card-body">
+      <h5 class="card-title whitespace-nowrap text-center">Drop a file here</h5>
+      <p class="card-text whitespace-nowrap text-center">Or click to select a file</p>
+    </div>
+
+    <!-- TODO action -->
+    <!-- <div class="card-actions justify-end">
+      <button class="btn btn-primary">Proceed</button>
+    </div> -->
+  </label>
+
+  <input type="file" bind:files id="file-input" class="hidden" />
+</div>
--- frontend/docjson/src/routes/+page.svelte
+++ frontend/docjson/src/routes/+page.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+  import FileDrop from '$lib/components/FileDrop.svelte'
+
+  let files: FileList | null = null
 </script>
 
 <section class="hero min-h-full flex-grow">
@@ -6,6 +9,11 @@
     <h1>Streamline your workflow with easy file to JSON conversions</h1>
     <p>Pick a document to create schema of</p>
     <!-- TODO file drop input -->
+
+    <div>
+      <FileDrop bind:files />
+    </div>
+
     <!-- TODO list of previous schemas -->
   </div>
 </section>
