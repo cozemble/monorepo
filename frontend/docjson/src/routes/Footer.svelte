b5c51a28bcb2f7be705162edcd471fa863c2d11e
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2023-08-10 16:41:23 +0300

    feat(docjson): setup footer

--- frontend/docjson/src/routes/Footer.svelte
+++ frontend/docjson/src/routes/Footer.svelte
@@ -1,9 +1,14 @@
-<footer class="footer">
-  <!-- TODO docs -->
-  <!-- TODO pricing -->
-  <!-- TODO about -->
-  <!-- TODO contact -->
+<footer class="footer px-10 py-5 bg-base-200 justify-between">
+  <div class="flex flex-row gap-6">
+    <a href="/docs/getting-started" class="link-hover">Getting Started</a>
+    <a href="/docs" class="link-hover">Docs</a>
+    <a href="/pricing" class="link-hover link-secondary">Pricing</a>
+  </div>
 
-  <!-- TODO terms -->
-  <!-- TODO privacy -->
+  <div class="flex flex-row gap-6">
+    <a href="/contact" class="link-hover">Contact</a>
+    <a href="/about" class="link-hover">About</a>
+    <a href="/privacy" class="link-hover">Privacy</a>
+    <a href="/terms" class="link-hover">Terms</a>
+  </div>
 </footer>
