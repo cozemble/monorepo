d5800d3864616cacdffd24dae9a2bc421907deb5
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2023-08-28 22:43:25 +0300

    feat(docjson): change base font family to Rubik

--- frontend/docjson/tailwind.config.cjs
+++ frontend/docjson/tailwind.config.cjs
@@ -3,8 +3,9 @@ module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ['"Rubik"', 'sans-serif'],
+    },
   },
-  plugins: [
-    require("@tailwindcss/typography"), require('daisyui')
-  ],
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
 }
