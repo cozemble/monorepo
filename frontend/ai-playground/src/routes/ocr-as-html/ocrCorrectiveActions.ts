676bc082f9a562b77da1a151044ca4633b95c4c8
mikehogan <me@mikehogan.net>
2023-08-28 19:37:38 +0100

    can label tables, merge tables and delete rows

--- frontend/ai-playground/src/routes/ocr-as-html/ocrCorrectiveActions.ts
+++ frontend/ai-playground/src/routes/ocr-as-html/ocrCorrectiveActions.ts
@@ -26,10 +26,17 @@ export const deleteRows = z.object({
   rowRegex: z.string().min(1, { message: 'Must be at least 1 character long' }),
 })
 
-export const action = z.discriminatedUnion('action', [labelTable, deleteRows])
+export const mergeTables = z.object({
+  action: z.literal('mergeTables'),
+  tableLabel: z.string().min(1, { message: 'Required' }),
+  tableHasHeader: z.boolean(),
+})
+
+export const action = z.discriminatedUnion('action', [labelTable, deleteRows, mergeTables])
 export const actions = action.array()
 
 export type LabelTable = z.infer<typeof labelTable>
 export type DeleteRows = z.infer<typeof deleteRows>
+export type MergeTables = z.infer<typeof mergeTables>
 export type Action = z.infer<typeof action>
 export type Actions = z.infer<typeof actions>
---
+++ frontend/cozemble-forms/src/lib/fom/components/FomBoolean.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import {getContext} from 'svelte'
+    import type {FomBoolean} from "../Fom";
+    import {componentFinder, type Path} from "./helper";
+
+    export let key: string
+    export let field: FomBoolean
+    export let value: boolean
+    export let path: Path
+
+    $: componentAndProps = componentFinder(getContext)(field, path)
+</script>
+
+<svelte:component this={componentAndProps.component} {...componentAndProps.props} {key} bind:value={value}/>
