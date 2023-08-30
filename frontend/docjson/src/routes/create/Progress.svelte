b5587405f97551aa0aa7c7b1b70654dc727b5d2a
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2023-08-30 18:03:06 +0300

    feat(docjson): create page Progress done icon

--- frontend/docjson/src/routes/create/Progress.svelte
+++ frontend/docjson/src/routes/create/Progress.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import Icon from '@iconify/svelte'
+
   export let message: string = 'Initializing the process'
   export let progress: number
 </script>
@@ -10,10 +12,17 @@
 <div class="progress-area w-full flex flex-col items-center justify-center ">
   <!-- message -->
   <p class="mb-4 text-center ">
+  <!-- TODO determine which orientation to use-->
+  <!-- <div class="flex gap-4"> -->
+  <p class="mb-4 text-center opacity-40">
     {message}
   </p>
 
-  <span class="loading loading-md loading-dots mb-4" />
+  <span class="swap swap-flip mb-6  hover:cursor-default" class:swap-active={progress >= 1}>
+    <Icon icon="subway:tick" class="swap-on text-2xl text-secondary" />
+    <span class="swap-off loading loading-md loading-dots" />
+  </span>
+  <!-- </div> -->
 
   <progress
     class="progress progress-secondary w-full h-3 justify-self-end transition ease-in"
