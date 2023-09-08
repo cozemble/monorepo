1d2b214cec5ea6522b7dd0892e2ee568b3950832
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2023-09-08 12:18:44 +0300

    fix(docsjon): EmailOnRelease z-index issue

--- frontend/docjson/src/routes/(landing)/pricing/EmailOnRelease.svelte
+++ frontend/docjson/src/routes/(landing)/pricing/EmailOnRelease.svelte
@@ -19,7 +19,7 @@
   <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
   <form
     tabindex="0"
-    class="dropdown-content p-4 w-[27em] max-w-[80vw] z-[1] glass bg-gray-500 rounded-box flex flex-col"
+    class="dropdown-content p-4 w-[27em] max-w-[80vw] z-30 glass bg-gray-500 rounded-box flex flex-col"
   >
     <span class="mb-3 text-sm font-normal leading-4 text-base-100/70 ">
       We will notify you via email when we release the first version of the app.</span
