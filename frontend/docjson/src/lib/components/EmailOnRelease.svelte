63e4c355d53baffb6b85307923f97f0a3d3acbc3
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2023-09-10 17:06:22 +0300

    feat(docjson): EmailOnRelease new style

--- frontend/docjson/src/lib/components/EmailOnRelease.svelte
+++ frontend/docjson/src/lib/components/EmailOnRelease.svelte
@@ -2,37 +2,51 @@
   import Icon from '@iconify/svelte'
 
   let value: string
+  let isOpen = false
+  let hasSubmitted = false
 
-  function submit() {
-    alert('You submitted: ' + value)
+  function handleSubmit() {
+    console.log('You submitted: ' + value)
   }
+
+  function mainHandler() {
+    if (!isOpen) return (isOpen = true)
+
+    handleSubmit()
+    isOpen = false
+    hasSubmitted = true
+  }
+
+  $: buttonText =
+    (isOpen && 'Join Waitlist') ||
+    (hasSubmitted && 'Thanks for joining!') ||
+    'Want to get notified?'
+
+  $: tooltipText =
+    (isOpen && 'We will notify you via email') ||
+    (hasSubmitted && 'You have joined the waitlist!') ||
+    'Get notified when we release the first version of tojson.'
+
+  $: buttonClass =
+    (isOpen && 'btn-primary join-item') || (hasSubmitted && 'btn-success') || 'btn-secondary'
+
+  $: tooltipClass =
+    (isOpen && 'tooltip-primary') || (hasSubmitted && 'tooltip-success') || 'tooltip-neutral'
 </script>
 
-<div class="dropdown dropdown-center">
-  <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-  <!-- svelte-ignore a11y-label-has-associated-control -->
-  <label tabindex="0" class="m-1 btn btn-secondary">
-    <Icon icon="mdi:bell" class="mr-2 text-xl" />
-    Want to be notified?</label
-  >
+<div class="tooltip {tooltipClass}" data-tip={tooltipText}>
+  <div class="join">
+    <input
+      type="email"
+      class="input input-primary join-item w-0 transition-all duration-200 
+            {isOpen ? 'w-64 visible ' : 'pointer-events-none p-0 border-0'}"
+      placeholder="Your email address"
+      bind:value
+    />
 
-  <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-  <form
-    tabindex="0"
-    class="dropdown-content p-4 w-[27em] max-w-[80vw] z-30 glass bg-gray-500 rounded-box flex flex-col"
-  >
-    <span class="mb-3 text-sm font-normal leading-4 text-base-100/70 ">
-      We will notify you via email when we release the first version of the app.</span
-    >
-
-    <div class="join">
-      <input
-        type="email"
-        class="input input-secondary join-item w-full"
-        placeholder="Your email address"
-        bind:value
-      />
-      <button class="btn join-item btn-secondary" on:click={submit}>submit</button>
-    </div>
-  </form>
+    <button class="pr-6 btn {buttonClass}" on:click={mainHandler}>
+      <Icon icon={hasSubmitted ? 'mdi:bell-check' : 'mdi:bell'} class="mr-1 text-xl" />
+      {buttonText}
+    </button>
+  </div>
 </div>
