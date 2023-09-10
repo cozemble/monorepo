73c6033a005e7010f93eae5864704e1b6c8757ca
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2023-09-10 17:41:41 +0300

    chore(docjson): `xs-only` custom tailwind class
    To apply styles in only xs devices

--- frontend/docjson/tailwind.config.cjs
+++ frontend/docjson/tailwind.config.cjs
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+import plugin from 'tailwindcss/plugin'
+
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
@@ -7,5 +10,11 @@ module.exports = {
       sans: ['"Rubik"', 'sans-serif'],
     },
   },
-  plugins: [require('@tailwindcss/typography'), require('daisyui')],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('daisyui'),
+    plugin(function ({ addVariant }) {
+      addVariant('xs-only', "@media screen and (max-width: theme('screens.sm'))")
+    }),
+  ],
 }
