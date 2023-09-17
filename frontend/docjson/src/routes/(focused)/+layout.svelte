5204bc30ffcb2d7a637afd146559e11884d6ec86
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2023-09-17 15:34:56 +0300

    feat(docjson): create focused route group & move create page in it

--- frontend/docjson/src/routes/(focused)/+layout.svelte
+++ frontend/docjson/src/routes/(focused)/+layout.svelte
@@ -3,7 +3,13 @@
   import NotificationProvider from '$lib/components/notifications/NotificationProvider.svelte'
 </script>
 
-<header class="navbar p-4">
+<!-- 
+  @component
+  Here in this 'focused' group we have the pages that are focused to it's content. 
+  We don't have the complex header and footer here. We only display the logo and the content of the page.
+ -->
+
+<header class="navbar p-4 sm:py-3 sm:px-6">
   <Logo />
 </header>
 
