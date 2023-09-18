1ea681a3595c3605e2c886bf3e4b36bf99dba775
mikehogan <me@mikehogan.net>
2023-09-18 14:43:06 +0100

    finally, remove my copy of vercel/ai

--- thirdparty/vercel-ai/packages/core/index.ts
+++
@@ -1,5 +0,0 @@
-import * as svelte from './svelte/index.js'
-
-export { OpenAIStream } from './streams/openai-stream'
-export { StreamingTextResponse } from './streams/streaming-text-response'
-export { svelte }
