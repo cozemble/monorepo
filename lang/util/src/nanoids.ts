35ec43305b37e56ab3f593f9b30901d2d1ab7c6a
mikehogan <me@mikehogan.net>
2023-09-18 14:29:01 +0100

    my own code depended on nanoid! Making it use the nonsecure version

--- lang/util/src/nanoids.ts
+++ lang/util/src/nanoids.ts
@@ -1,4 +1,4 @@
-import { customAlphabet } from 'nanoid'
+import { customAlphabet } from 'nanoid/non-secure'
 const alphaNumeric = customAlphabet('1234567890abcdefghijklmnopqrstuvwxyz', 20)
 export const nanoids = {
   alpha: () => alphaNumeric(),
