d5c715e6db55a86d237690f7ac78f5f7ce8fb10c
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2023-09-23 13:52:37 +0300

    feat(docjson): landing hero features list instead of subheading

--- frontend/docjson/src/routes/(landing)/+page.svelte
+++ frontend/docjson/src/routes/(landing)/+page.svelte
@@ -1,13 +1,21 @@
 <script lang="ts">
   import EmailOnRelease from '$lib/components/EmailOnRelease.svelte'
+  import Icon from '@iconify/svelte'
   import TryItForm from './TryItForm.svelte'
+
+  const features = [
+    ['Handles', ' horrible, complex documents. Ugly docs, clean JSON'],
+    ['No coding required.', ' We use wizards and AI'],
+    ['You define', ' the JSON you want'],
+    ['Send it where you want.', ' Zapier, Google sheets, your app - your choice'],
+  ]
 </script>
 
 <section class="hero min-h-full flex-grow">
   <div class="hero-content flex flex-col md:flex-row gap-12 lg:gap-[10em]">
     <div class="flex-grow-0 min-w">
       <h1
-        class="mb-6 xl:mb-8 relative xl:text-5xl font-bold 
+        class="mb-8 relative xl:text-5xl font-bold 
         !leading-[1.2] tracking-tighter [word-spacing:0.06em]"
       >
         Quickly and easily create <span
@@ -26,13 +34,35 @@
           files to JSON
         </span>
       </h1>
-      <p
+
+      <!-- Subheading - disabled -->
+      <!-- <p
         class="mb-4 sm:mb-12 opacity-60 lg:text-lg leading-relaxed tracking-tighter [word-spacing:0.1em]
       "
       >
         Auto-read data from your <span class="text-secondary">documents or text data</span> and organize
         it in the structure you want, with help from AI.
-      </p>
+      </p> -->
+
+      <!-- Features as a list -->
+      <ul class="mb-12">
+        {#each features as feat}
+          <li>
+            <p
+              class="opacity-60 leading-none tracking-tighter [word-spacing:0.1em] py-1
+          "
+            >
+              <Icon icon="mdi:check" class="text-secondary text-lg inline mr-2" />
+
+              <span class="text-primary tracking-tighter font-medium [word-spacing:0.1em]">
+                {feat[0]}
+              </span>
+
+              {feat[1]}
+            </p>
+          </li>
+        {/each}
+      </ul>
 
       <EmailOnRelease />
     </div>
