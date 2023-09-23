e7b3ba43bd93ba9541f9c8534126957d5bfb57ce
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2023-09-23 15:06:47 +0300

    feat(docjson): basic design of the sign-in page
    - Login form
    - Signup form
    - Social login

---
+++ frontend/docjson/src/routes/(focused)/sign-in/SignupForm.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+  export let switchForm: () => void
+</script>
+
+<!-- TODO functionality -->
+
+<form action="/" method="post" class="card">
+  <div class="form-control w-full mb-2">
+    <label for="email" class="label">Name</label>
+    <input
+      type="text"
+      id="name"
+      name="name"
+      class="input input-bordered w-full"
+      placeholder="Your Name"
+    />
+  </div>
+
+  <div class="form-control w-full mb-2">
+    <label for="email" class="label">Email</label>
+    <input
+      type="email"
+      id="email"
+      name="email"
+      class="input input-bordered w-full"
+      placeholder="you@example.com"
+    />
+  </div>
+
+  <div class="form-controll w-full">
+    <label for="password" class="label">Password</label>
+    <input
+      type="password"
+      id="password"
+      name="password"
+      class="input input-bordered w-full"
+      placeholder="••••••••"
+    />
+  </div>
+
+  <div class="form-control mt-4">
+    <label class="label cursor-pointer">
+      <input type="checkbox" class="checkbox checkbox-primary" />
+      <span class="label-text">I accept the privacy policy and terms of use </span>
+    </label>
+  </div>
+
+  <div class="flex flex-col items-center mt-6">
+    <button type="submit" class="btn btn-primary w-full">Sign in</button>
+    <button class="link-hover pt-2 text-sm opacity-50 hover:opacity-70" on:click={switchForm}>
+      already have an account?
+    </button>
+  </div>
+</form>
---
+++ frontend/docjson/src/routes/(focused)/sign-in/SocialSignIn.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import Icon from '@iconify/svelte'
+</script>
+
+<!-- TODO functionality -->
+
+<!-- Social sign in buttons -->
+<div class=" flex flex-col gap-3">
+  <!-- Sign in with GitHub button -->
+  <button class="btn bg-gray-700 hover:bg-black text-base-100 w-full">
+    <!-- <Icon icon="mdi:github" class="w-6 h-6" /> -->
+    <Icon icon="line-md:github-loop" class="w-6 h-6" />
+    <span>Continue with GitHub</span>
+  </button>
+
+  <!-- Sign in with Linkedin button -->
+  <button class="btn bg-[#0072b1] text-base-100 w-full">
+    <!-- <Icon icon="logos:linkedin-icon" class="w-6 h-6" /> -->
+    <Icon icon="line-md:linkedin" class="w-6 h-6" />
+    <span>Continue with Linkedin</span>
+  </button>
+
+  <!-- Sign in with Google button -->
+  <button class="btn  w-full">
+    <Icon icon="logos:google-icon" class="w-6 h-6" />
+    <span>Continue with Google</span>
+  </button>
+
+  <!-- SSO button -->
+  <button class="btn btn-link text-neutral w-full">
+    <Icon icon="mdi:lock-outline" class="w-6 h-6" />
+    <span>Continue with SSO</span>
+  </button>
+</div>
