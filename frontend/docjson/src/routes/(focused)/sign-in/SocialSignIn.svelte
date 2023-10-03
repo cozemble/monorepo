43770c564b2180a2a87167712bf00dc71bd2be4c
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2023-10-03 18:55:16 +0300

    feat(docjson): SocialSignIn callback for session

--- frontend/docjson/src/routes/(focused)/sign-in/SocialSignIn.svelte
+++ frontend/docjson/src/routes/(focused)/sign-in/SocialSignIn.svelte
@@ -1,14 +1,19 @@
 <script lang="ts">
+  import { page } from '$app/stores'
   import Icon from '@iconify/svelte'
   import type { Provider, SupabaseClient } from '@supabase/supabase-js'
 
-  export let supabase: SupabaseClient;
+  export let supabase: SupabaseClient
 
   const handleOAuthLogin = async (provider: Provider) => {
-    let { error } = await supabase.auth.signInWithOAuth({ provider })
+    let { error } = await supabase.auth.signInWithOAuth({
+      provider,
+      options: {
+        redirectTo: $page.url.origin + '/sign-in/callback',
+      },
+    })
     if (error) console.log('OAuth Error:', error.message)
   }
-
 </script>
 
 <!-- TODO functionality -->
@@ -16,14 +21,20 @@
 <!-- Social sign in buttons -->
 <div class=" flex flex-col gap-3">
   <!-- Sign in with GitHub button -->
-  <button class="btn bg-gray-700 hover:bg-black text-base-100 w-full" on:click={() => handleOAuthLogin('github')}>
+  <button
+    class="btn bg-gray-700 hover:bg-black text-base-100 w-full"
+    on:click={() => handleOAuthLogin('github')}
+  >
     <!-- <Icon icon="mdi:github" class="w-6 h-6" /> -->
     <Icon icon="line-md:github-loop" class="w-6 h-6" />
     <span>Continue with GitHub</span>
   </button>
 
   <!-- Sign in with Linkedin button -->
-  <button class="btn bg-[#0072b1] hover:bg-[#07537c] text-base-100 w-full" on:click={() => handleOAuthLogin('linkedin')}>
+  <button
+    class="btn bg-[#0072b1] hover:bg-[#07537c] text-base-100 w-full"
+    on:click={() => handleOAuthLogin('linkedin')}
+  >
     <!-- <Icon icon="logos:linkedin-icon" class="w-6 h-6" /> -->
     <Icon icon="line-md:linkedin" class="w-6 h-6" />
     <span>Continue with Linkedin</span>
