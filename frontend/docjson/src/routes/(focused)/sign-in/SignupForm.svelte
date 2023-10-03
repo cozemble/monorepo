29b177e6095ee90df348cfb31e647e1548963d32
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2023-10-03 18:54:28 +0300

    feat(docjson): SignupForm validation

--- frontend/docjson/src/routes/(focused)/sign-in/SignupForm.svelte
+++ frontend/docjson/src/routes/(focused)/sign-in/SignupForm.svelte
@@ -1,48 +1,146 @@
 <script lang="ts">
-  import { enhance } from '$app/forms'
+  import type { EventHandler } from 'svelte/elements'
+  import { writable } from 'svelte/store'
+  import { z } from 'zod'
+
   import FormInput from '$lib/components/form/FormInput.svelte'
+  import notifications from '$lib/stores/notifications'
 
   export let switchForm: () => void
   export let form: Record<string, string>
 
-  let helperText: HelperText = { error: false, text: null };
+  let helperText: HelperText = { error: false, text: null }
 
+  let values = {
+    name: '',
+    email: '',
+    password: '',
+    term: false,
+  }
+  $: if (form?.name) values.name = form.name
+  $: if (form?.email) values.email = form.email
+
+  $: console.log(values)
+
+  //
+
+  const Schema = z.object({
+    name: z
+      .string()
+      .min(4, 'Name should be longer than 4 characters')
+      .max(24, 'Name should be shorter than 24 characters'),
+    email: z.string().email('Please provide a valid email address'),
+    password: z.string().min(8, 'Password should be minimum 8 characters'),
+    term: z.literal(true, {
+      errorMap: () => ({ message: 'You must accept the terms & conditions' }),
+    }),
+  })
+
+  let errors = writable<{
+    name?: string
+    email?: string
+    password?: string
+    term?: string
+  }>({})
+
+  //
+
+  const onSubmit: EventHandler = async () => {
+    errors.set({})
+
+    const validation = Schema.safeParse(values)
+
+    if (!validation.success) {
+      const errs = validation.error.flatten().fieldErrors
+
+      console.error(errs)
+
+      errors.set({
+        name: errs.name?.[0],
+        email: errs.email?.[0],
+        password: errs.password?.[0],
+        term: errs.term?.[0],
+      })
+
+      notifications.create({
+        text: 'Error',
+        description: 'Please check the form for errors',
+        type: 'error',
+        duration: 2000,
+      })
+
+      return
+    }
+
+    // mock the behavior for now
+    // <!-- TODO actual sign up -->
+
+    console.warn(validation.data)
+
+    const notification = notifications.create({
+      text: 'Signing up',
+      description: 'Please wait while we sign you up',
+      type: 'loading',
+      isClosable: false,
+    })
+
+    setTimeout(() => {
+      notification.handleClose()
+      notifications.create({
+        text: 'Success',
+        description: 'You have been signed up successfully',
+        type: 'success',
+        duration: 2000,
+      })
+    }, 2000)
+  }
 </script>
 
-<!-- TODO functionality -->
 <!-- TODO display check your email after submit -->
 
-<!-- TODO handle error message-->
 <!-- svelte-ignore empty-block -->
-{#if !!helperText.text}
-{/if}
+{#if !!helperText.text}{/if}
 
-<form action="?/signup" method="post" class="card" use:enhance>
-
-  <FormInput name="name" value={form?.name ?? ''} placeholder="Your Name" />
+<form>
+  <FormInput name="name" bind:value={values.name} error={$errors?.name} placeholder="Your Name" />
 
   <FormInput
     name="email"
-    value={form?.email ?? ''}
+    bind:value={values.email}
+    error={$errors?.email}
     placeholder="you@example.com"
   />
 
   <FormInput
     name="password"
-    value={form?.password ?? ''}
+    bind:value={values.password}
+    error={$errors?.password}
     placeholder="••••••••"
     type="password"
   />
 
-  <div class="form-control mt-4">
+  <div class="form-control">
     <label class="label cursor-pointer">
-      <input type="checkbox" name="term" class="checkbox checkbox-primary" />
+      <input
+        type="checkbox"
+        name="term"
+        bind:checked={values.term}
+        class="checkbox checkbox-primary"
+      />
       <span class="label-text">I accept the privacy policy and terms of use </span>
+    </label>
+
+    <label for="term" class="label text-error pb-0 pt-1">
+      <div />
+      <label for="" class="label-text-alt text-error">
+        {$errors?.term || ''}
+      </label>
     </label>
   </div>
 
   <div class="flex flex-col items-center mt-6">
-    <button type="submit" class="btn btn-primary w-full">Sign in</button>
+    <button type="submit" class="btn btn-primary w-full" on:click={onSubmit}>Sign in</button>
+
     <button class="link-hover pt-2 text-sm opacity-50 hover:opacity-70" on:click={switchForm}>
       already have an account?
     </button>
