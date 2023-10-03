cd2725284712c8223d48e9af66e0ad93d5f3c3b4
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2023-10-03 18:42:13 +0300

    feat(docjson): FormInput value binding

--- frontend/docjson/src/lib/components/form/FormInput.svelte
+++ frontend/docjson/src/lib/components/form/FormInput.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   export let name: string
   /** Default: text */
-  export let type: 'text' | 'password' | 'email' = 'text'
+  export let type: 'text' | 'password' = 'text'
   export let placeholder: string = ''
   export let value: string = ''
   export let error: string | undefined = undefined
@@ -13,19 +13,31 @@
     - Displays error message if error prop is passed
  -->
 
-<div class="form-control w-full mb-2 indicator">
+<div class="form-control w-full indicator">
   <label for={name} class="label capitalize">{name}</label>
 
-  <input
-    {type}
-    id={name}
-    {name}
-    class="input input-bordered w-full {error && 'input-error'}"
-    {placeholder}
-    {value}
-  />
+  <!-- Svelte doesn't allow value binding when the type is dynamic -->
+  {#if type === 'password'}
+    <input
+      type="password"
+      id={name}
+      {name}
+      class="input input-bordered w-full {error && 'input-error'}"
+      {placeholder}
+      bind:value
+    />
+  {:else}
+    <input
+      type="text"
+      id={name}
+      {name}
+      class="input input-bordered w-full {error && 'input-error'}"
+      {placeholder}
+      bind:value
+    />
+  {/if}
 
-  <label for={name} class="label text-error pb-0 pt-1">
+  <label for={name} class="label text-error py-1">
     <div />
     <label for="" class="label-text-alt text-error">
       {error || ''}
