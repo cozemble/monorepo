dc450a4e04af4929c2ebb410a27cc3c8f759d561
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2023-10-08 22:46:36 +0300

    feat(docjson): make auth pages background smoother
    It was cluttering the design

--- frontend/docjson/src/routes/(focused)/auth/+layout.svelte
+++ frontend/docjson/src/routes/(focused)/auth/+layout.svelte
@@ -12,8 +12,8 @@
 <!-- bg-gradient-to-b from-primary/20 to-accent/50 text-base-content -->
 
 <!-- Background decoration -->
-<div class="absolute top-1/2 left-2/3 w-32 h-32 -z-40 bg-primary/80 blur-3xl " />
-<div class="absolute top-2/3 left-1/2 w-32 h-32 -z-40 bg-secondary/60 blur-3xl " />
-<div class="absolute top-1/3 left-1/3 w-32 h-32 -z-40 bg-accent/80 blur-3xl " />
+<div class="absolute top-1/2 left-2/3 w-32 h-32 -z-40 bg-primary/80 blur-[20em] " />
+<div class="absolute top-2/3 left-1/2 w-32 h-32 -z-40 bg-secondary/60 blur-[20em] " />
+<div class="absolute top-1/3 left-1/3 w-32 h-32 -z-40 bg-accent/80 blur-[20em] " />
 
 <slot />
