f613fe279354701602e5df64d36576c93a74643a
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2023-10-14 11:35:33 +0300

    feat(docjson): sign-out page

--- frontend/docjson/src/routes/(focused)/auth/+layout.svelte
+++ frontend/docjson/src/routes/(focused)/auth/+layout.svelte
@@ -3,10 +3,15 @@
   import { goto } from '$app/navigation'
 
   import user from '$lib/stores/user'
+  import { page } from '$app/stores'
 
   // <!-- TODO make sure if this is a good way to handle access -->
 
   onMount(() => {
+    const path = $page.url.pathname
+
+    if (path === '/auth/sign-out') return
+
     if (!$user.isGuest) goto('/dashboard')
   })
 </script>
