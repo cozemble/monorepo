3920dea8e7097a8e3a9e632941180f11caebb88c
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2023-10-15 13:28:29 +0300

    feat(docjson): user store user details
    And some refactoring

--- frontend/docjson/src/lib/stores/user.ts
+++ frontend/docjson/src/lib/stores/user.ts
@@ -1,45 +1,53 @@
-// TODO user settings
-// TODO user API keys
-
-import { writable, type Readable, derived, get } from 'svelte/store'
+import { type Readable, derived } from 'svelte/store'
 import type { Session } from '@supabase/supabase-js'
 import { page } from '$app/stores'
 
 interface UserDetails {
   id: string
   name: string
-  email: string
-  // TODO rest of the details
-}
-interface User {
-  session: Session | null
-  isGuest: boolean
-  details: UserDetails
+  email?: string
 }
 
-//
+type User =
+  | {
+      session: Session
+      isGuest: false
+      details: UserDetails
+    }
+  | {
+      session: null
+      isGuest: true
+      details: null
+    }
 
-// TODO fetch user details from supabase
-const details = writable<UserDetails>({
-  id: 'mock-user',
-  name: 'Mock User',
-  email: 'mock@user.com',
-})
+// TODO user settings
 
 /**
- * A unified store for user data that combines:
+ * A utility store to get the user details
  * - session: session data from supabase
  * - isGuest: true if session is null
  * - details: user details
  */
-const user: Readable<User> = derived([page, details], ([$page, $details]) => {
+const user: Readable<User> = derived([page], ([$page]) => {
   const session = $page?.data?.session ?? null
-  const isGuest = session ? false : true
+
+  if (!session)
+    return {
+      session,
+      isGuest: true as const,
+      details: null,
+    }
+
+  const details = {
+    id: session.user.id,
+    name: session.user.user_metadata.name as string,
+    email: session.user.email,
+  }
 
   return {
     session,
-    isGuest,
-    details: $details,
+    isGuest: false as const,
+    details,
   }
 })
 
