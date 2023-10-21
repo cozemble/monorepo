1f5b75de49dede197ac3d8452983b615d8cafd95
Oyewole S. Abayomi <oyewoleabayomi@gmail.com>
2023-10-21 17:23:13 +0100

    fix: revert changes

--- frontend/docjson/src/routes/(focused)/auth/callback/+server.ts
+++ frontend/docjson/src/routes/(focused)/auth/callback/+server.ts
@@ -4,7 +4,7 @@ import { redirect } from '@sveltejs/kit'
 export const GET = async ({ url, locals: { supabase } }) => {
   const code = url.searchParams.get('code')
   const redirectTo = url.searchParams.get('redirectTo')
-  
+
   if (code) {
     await supabase.auth.exchangeCodeForSession(code)
     throw redirect(303, redirectTo || '/dashboard')
