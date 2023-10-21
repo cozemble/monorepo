6d9c1afaf8445c1af5f0eabe12757ecc5b41be43
Oyewole S. Abayomi <oyewoleabayomi@gmail.com>
2023-10-21 15:57:56 +0100

    fix: restructure callback url

--- frontend/docjson/src/routes/(focused)/auth/v1/callback/+server.ts
+++ frontend/docjson/src/routes/(focused)/auth/v1/callback/+server.ts
@@ -1,9 +1,10 @@
 import { redirect } from '@sveltejs/kit'
 
+// http://tojson.dev/auth/v1/callback
 export const GET = async ({ url, locals: { supabase } }) => {
   const code = url.searchParams.get('code')
   const redirectTo = url.searchParams.get('redirectTo')
-
+  
   if (code) {
     await supabase.auth.exchangeCodeForSession(code)
     throw redirect(303, redirectTo || '/dashboard')
