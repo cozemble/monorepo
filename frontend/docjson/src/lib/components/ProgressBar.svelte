8e031b5b8db714ef92e9cdab9cceee03fd96317a
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2023-10-24 17:27:47 +0300

    feat(docjson): ProgressBar improved message styling

--- frontend/docjson/src/lib/components/ProgressBar.svelte
+++ frontend/docjson/src/lib/components/ProgressBar.svelte
@@ -21,20 +21,18 @@
 -->
 
 <div class="progress-area w-full flex flex-col items-center justify-center ">
-  <!-- TODO determine which orientation to use-->
+  <p class="opacity-60 mb-4">
+    {#if message}
+      <span class="mb-4 text-center italic">
+        {message}
+      </span>
+    {/if}
 
-  <!-- <div class="flex gap-4"> -->
-  {#if message}
-    <p class="mb-4 text-center opacity-40">
-      {message}
-    </p>
-  {/if}
-
-  <span class="swap swap-flip mb-6  hover:cursor-default" class:swap-active={progress >= 1}>
-    <Icon icon="subway:tick" class="swap-on text-2xl text-secondary" />
-    <span class="swap-off loading loading-md loading-dots" />
-  </span>
-  <!-- </div> -->
+    <span class="swap swap-flip ml-2 hover:cursor-default" class:swap-active={progress >= 1}>
+      <Icon icon="subway:tick" class="swap-on text-secondary" />
+      <span class="swap-off loading loading-sm loading-dots" />
+    </span>
+  </p>
 
   <progress
     class="progress progress-secondary w-full h-3 justify-self-end transition ease-in"
